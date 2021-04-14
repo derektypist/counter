33ee6d15b13b5518422b424569f35d302941e809
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2021-04-14 09:39:04 +0000

    Added forEach loop at script.js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,8 @@ let count = 0;
 const value = document.querySelector('#value');
 const btns = document.querySelectorAll('.btn');
 
+btns.forEach(function(btn) {
+    btn.addEventListener('click',function(e) {
+        
+    });
+});
