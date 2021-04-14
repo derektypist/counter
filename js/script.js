4d83ccae8c244a0303e336d858fd3286df412e31
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2021-04-14 09:47:37 +0000

    Added conditional formatting at script.js

--- js/script.js
+++ js/script.js
@@ -7,6 +7,34 @@ const btns = document.querySelectorAll('.btn');
 
 btns.forEach(function(btn) {
     btn.addEventListener('click',function(e) {
-        
+        const styles = e.currentTarget.classList;
+        if (styles.contains('decrease')) {
+            count--;
+        }
+        else if (styles.contains('increase')) {
+            count++;
+        }
+        else {
+            count = 0;
+        }
+
+        // Apply Conditional Formatting According to value of count
+
+        // Set color to green if count > 0
+        if (count > 0) {
+            value.style.color = "green";
+        }
+
+        // Set color to red if count < 0
+        if (count < 0) {
+            value.style.color = "red";
+        }
+
+        // Set color to black if count equals 0
+        if (count == 0) {
+            value.style.color = "black";
+        }
+
+        value.textContent = count;
     });
 });
